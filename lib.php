a542c24a331eaf41850171b285bc986fcab4f16f
PolinaAndreyanova <andreyanovalina@yandex.ru>
2024-07-04 21:10:47 +0000

    внести правки в код-стайл, исправить мелкие недостатки визуализации вывода

--- lib.php
+++ lib.php
@@ -13,188 +13,186 @@ function printR(array $arData): void
     echo "</pre>";
 }
 
+function handlerSerializedString(string $string): array
+{
+    $arString = str_split($string);
+    $arDataSplit = [];
+    $isChange = false;
+    $isAddType = false;
+    $c = 0;
+
+    foreach ($arString as $k => $v) {
+        if ($v == "O" and $arString[$k + 1] == ":") {
+            $isChange = !$isChange;
+
+            $arDataSplit[] = "a";
+            $arDataSplit[] = ":";
+            $arDataSplit[] = "1";
+            $arDataSplit[] = ":";
+            $arDataSplit[] = "{";
+            $arDataSplit[] = "s";
+
+            $isAddType = true;
+            $c = 0;
+        } elseif ($v == ":" and $arString[$k - 1] == '"' and $isChange) {
+            $isChange = !$isChange;
+
+            $arDataSplit[] = ";";
+            $arDataSplit[] = "a";
+            $arDataSplit[] = ":";
+        } else {
+            $arDataSplit[] = $v;
+            
+            if ($isAddType and $v === ":") {
+                $c += 1;
+            } elseif ($isAddType and $v === '"' and $c === 2) {
+                $arDataSplit[] = "O";
+                $arDataSplit[] = "B";
+                $arDataSplit[] = "J";
+                $arDataSplit[] = "E";
+                $arDataSplit[] = "C";
+                $arDataSplit[] = "T";
+                $arDataSplit[] = "_";
+
+                $isAddType = false;
+                $c = 0;
+            } 
+        }
+    }
+
+    return $arDataSplit;
+}
+
 function deserialize(string $data): array
 {
-    // $arObjects = preg_split("/O:[0-9]*:/", $data);
     $arDeserializedData = [];
+    $arDataSplit = handlerSerializedString($data);
 
-    // foreach ($arObjects as $object) {
-    //     if (!($object === "")) {
-            // $arObjectData = explode(":", $object, 3);
+    $curLevel = &$arDeserializedData;
+    $arLevels = [];
 
-            // $objectName = $arObjectData[0];
-            // $countProp = $arObjectData[1];
-
-            $preobrazovanie = str_split($data);
-            $arObjectPropSplit = [];
-            $isMeniaem = false;
-            foreach ($preobrazovanie as $k => $v) {
-                if ($v == "O" and $preobrazovanie[$k + 1] == ":") {
-                    $isMeniaem = !$isMeniaem;
-
-                    $arObjectPropSplit[] = "a";
-                    $arObjectPropSplit[] = ":";
-                    $arObjectPropSplit[] = "1";
-                    $arObjectPropSplit[] = ":";
-                    $arObjectPropSplit[] = "{";
-                    $arObjectPropSplit[] = "s";
-                } else if ($v == ":" and $preobrazovanie[$k - 1] == '"' and $isMeniaem) {
-                    $isMeniaem = !$isMeniaem;
-
-                    $arObjectPropSplit[] = ";";
-                    $arObjectPropSplit[] = "a";
-                    $arObjectPropSplit[] = ":";
-                } else {
-                    $arObjectPropSplit[] = $v;
-                }
-            }
-
-            // return $arObjectPropSplit;
-
-
-            $arObjectProp = [];
-            // $arObjectPropSplit = str_split($data);
-
-            $curLevel = &$arObjectProp;
-            $arLevels = [];
-
-            $curDataType = "";
-            $newKey = "";
-            $arCurDataValueSplit = [];
-            
-            $isOpenBracket = false;
-            
-            $isCount = false;
-            $countArElems = [];
-
-            $isEmptyArray = false;
-
-            foreach ($arObjectPropSplit as $number => $symbol) {
-                // return [];
-                if ($isOpenBracket) {
-                    $arCurDataValueSplit[] = $symbol;
-
-                    if ($symbol === ")") {
-                        $isOpenBracket = !$isOpenBracket;
-                    }
-                } else {
-                    if (in_array($symbol, ["s", "i", "d"]) and $arObjectPropSplit[$number + 1] === ":") {
-                        $curDataType = $symbol;
-                        $arCurDataValueSplit[] = $symbol;
-                    } else if ($symbol === "a" and $arObjectPropSplit[$number + 1] === ":") {
-                        $curDataType = $symbol;
-                        $arCurDataValueSplit[] = $symbol;
-                        
-                        if ($arObjectPropSplit[$number + 2] === "0") {
-                            $isEmptyArray = !$isEmptyArray;
-                        } else {
-                            // $curDataType = $symbol;
-                            // $arCurDataValueSplit[] = $symbol;
+    $curDataType = "";
+    $newKey = "";
+    $arCurDataValueSplit = [];
     
-                            $arLevels[] = &$curLevel;
-                            $curLevel = &$curLevel[$newKey];
-                            
-                            $newKey = "";
-                            
-                            $countArElems[] = 0;
-                        }
-                    } else if ($symbol === ":") {
-                        if (
-                            ($arObjectPropSplit[$number + 1] === "/" and $arObjectPropSplit[$number + 2] === "/")
-                            or in_array($arObjectPropSplit[$number + 1], ["=", ";"])
-                        ) {
-                            $arCurDataValueSplit[] = $symbol;
-                        } else if (in_array($curDataType, ["s", "i", "d"])) {
-                            $arCurDataValueSplit[] = $symbol;
-                        } else {
-                            $arCurDataValueSplit[] = $symbol;
-                            
-                            if ($arObjectPropSplit[$number - 1] === "a" and $arObjectPropSplit[$number + 1] !== "0") {
-                                $isCount = true;
-                                // echo implode($arCurDataValueSplit)."START<br>";
-                            } else {
-                                $isCount = false;
-                                // echo implode($arCurDataValueSplit)."END<br>";
-                            }
-                        }
-                    } else if ($symbol === ";" or ($isEmptyArray and $symbol === "}")) {
-                        // echo implode($arCurDataValueSplit)."<br>";
-                        if (
-                            !(in_array($arObjectPropSplit[$number + 1], ["a", "s", "i", "d", "N", "}"]) 
-                            or (!isset($arObjectPropSplit[$number + 1])))
-                        ) {
-                            $arCurDataValueSplit[] = $symbol;
-                        } else {
-                            $curValue = implode($arCurDataValueSplit);
-                            
-                            $arCurDataValueSplit = [];
+    $isOpenBracket = false;
+    
+    $isCount = false;
+    $countArElems = [];
 
-                            $arCurValue = [];
-                            
-                            if ($curDataType === "s") {
-                                $arCurValue = preg_split("/s:[0-9]*:/", $curValue);
-                            } else if (in_array($curDataType, ["i", "d"])) {
-                                $arCurValue = preg_split("/[id]:/", $curValue);
-                            } else if ($isEmptyArray) {
-                                $arCurValue = preg_split("/a:0:/", $curValue);
-                                $isEmptyArray = !$isEmptyArray;
-                            }
-                            
-                            $curDataType = "";
-                            
-                            if ($newKey === "") {
-                                $newKey = $arCurValue[array_key_last($arCurValue)];
-                                
-                                $curLevel[$newKey] = [];
-                            } else {
-                                $curLevel[$newKey] = $arCurValue[array_key_last($arCurValue)];
-                                
-                                $newKey = "";
-                                
-                                $countArElems[array_key_last($countArElems)] -= 1;
+    $isEmptyArray = false;
 
-                                // printR($countArElems);
-                                
-                                while ($countArElems[array_key_last($countArElems)] === 0) {
-                                    $curLevel = &$arLevels[array_key_last($arLevels)];
-                                    
-                                    unset($arLevels[array_key_last($arLevels)]);
-                                    unset($countArElems[array_key_last($countArElems)]);
-                                    
-                                    if ($countArElems[array_key_last($countArElems)] >= 1) {
-                                        $countArElems[array_key_last($countArElems)] -= 1;
-                                    }
-                                }
-                            }
-                        }
-                    } else if (in_array($symbol, ["(", ")"])) {
-                        $isOpenBracket = !$isOpenBracket;
-                        
-                        $arCurDataValueSplit[] = $symbol;
+    foreach ($arDataSplit as $number => $symbol) {
+        if ($isOpenBracket) {
+            $arCurDataValueSplit[] = $symbol;
+
+            if ($symbol === ")") {
+                $isOpenBracket = !$isOpenBracket;
+            }
+        } else {
+            if (in_array($symbol, ["s", "i", "d"]) and $arDataSplit[$number + 1] === ":") {
+                $curDataType = $symbol;
+                $arCurDataValueSplit[] = $symbol;
+            } elseif ($symbol === "a" and $arDataSplit[$number + 1] === ":") {
+                $curDataType = $symbol;
+                $arCurDataValueSplit[] = $symbol;
+                
+                if ($arDataSplit[$number + 2] === "0") {
+                    $isEmptyArray = !$isEmptyArray;
+                } else {
+                    $arLevels[] = &$curLevel;
+                    $curLevel = &$curLevel[$newKey];
+
+                    $newKey = "";
+                    
+                    $countArElems[] = 0;
+                }
+            } elseif ($symbol === ":") {
+                if (
+                    ($arDataSplit[$number + 1] === "/" and $arDataSplit[$number + 2] === "/")
+                    or in_array($arDataSplit[$number + 1], ["=", ";"])
+                ) {
+                    $arCurDataValueSplit[] = $symbol;
+                } elseif (in_array($curDataType, ["s", "i", "d"])) {
+                    $arCurDataValueSplit[] = $symbol;
+                } else {
+                    $arCurDataValueSplit[] = $symbol;
+                    
+                    if ($arDataSplit[$number - 1] === "a" and $arDataSplit[$number + 1] !== "0") {
+                        $isCount = true;
                     } else {
-                        if ($symbol === "N" and $arObjectPropSplit[$number + 1] === ";") {
-                            $curDataType = "s";
-                            $arCurDataValueSplit = ["s", ":", "1", ":"];    
-                        }
+                        $isCount = false;
+                    }
+                }
+            } elseif ($symbol === ";" or ($isEmptyArray and $symbol === "}")) {
+                if (
+                    !(in_array($arDataSplit[$number + 1], ["a", "s", "i", "d", "N", "}"]) 
+                    or (!isset($arDataSplit[$number + 1])))
+                ) {
+                    $arCurDataValueSplit[] = $symbol;
+                } else {
+                    $curValue = implode($arCurDataValueSplit);
+
+                    $arCurDataValueSplit = [];
+
+                    $arCurValue = [];
+
+                    if ($curDataType === "s") {
+                        $arCurValue = preg_split("/s:[0-9]*:/", $curValue);
+                    } elseif (in_array($curDataType, ["i", "d"])) {
+                        $arCurValue = preg_split("/[id]:/", $curValue);
+                    } elseif ($isEmptyArray) {
+                        $curValue[-1] = "[";
+                        $curValue .= "]";                        
+
+                        $arCurValue = preg_split("/a:0:/", $curValue);
+
+                        $isEmptyArray = !$isEmptyArray;
+                    }
+                    
+                    $curDataType = "";
+                    
+                    if ($newKey === "") {
+                        $newKey = $arCurValue[array_key_last($arCurValue)];
                         
-                        $arCurDataValueSplit[] = $symbol;
+                        $curLevel[$newKey] = [];
+                    } else {
+                        $curLevel[$newKey] = $arCurValue[array_key_last($arCurValue)];
                         
-                        if ($isCount) {
-                            // echo $countArElems[array_key_last($countArElems)]. "+" . intval($symbol) ."<br>";
-                            $countArElems[array_key_last($countArElems)] = $countArElems[array_key_last($countArElems)] * 10 + intval($symbol);
+                        $newKey = "";
+                        
+                        $countArElems[array_key_last($countArElems)] -= 1;
+                        
+                        while ($countArElems[array_key_last($countArElems)] === 0) {
+                            $curLevel = &$arLevels[array_key_last($arLevels)];
+                            
+                            unset($arLevels[array_key_last($arLevels)]);
+                            unset($countArElems[array_key_last($countArElems)]);
+                            
+                            if ($countArElems[array_key_last($countArElems)] >= 1) {
+                                $countArElems[array_key_last($countArElems)] -= 1;
+                            }
                         }
                     }
                 }
+            } elseif (in_array($symbol, ["(", ")"])) {
+                $isOpenBracket = !$isOpenBracket;
+                
+                $arCurDataValueSplit[] = $symbol;
+            } else {
+                if ($symbol === "N" and $arDataSplit[$number + 1] === ";") {
+                    $curDataType = "s";
+                    $arCurDataValueSplit = ["s", ":", "1", ":"];    
+                }
+                
+                $arCurDataValueSplit[] = $symbol;
+                
+                if ($isCount) {
+                    $countArElems[array_key_last($countArElems)] = $countArElems[array_key_last($countArElems)] * 10 + intval($symbol);
+                }
             }
+        }
+    }
 
-            // $arDeserializedData[] = [
-            //     "objectName" => $objectName,
-            //     "countProperties" => $countProp,
-            //     "properties" => $arObjectProp
-            // ];
-    //     }
-    // }
-
-    // return $arDeserializedData;
-    return $arObjectProp;
+    return $arDeserializedData;
 }
